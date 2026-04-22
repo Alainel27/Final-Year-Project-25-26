92954d60017f0adde28efeecc35276a1186d9feb
Alan Z <20102420@wit.ie>
2026-04-22 10:11:40 +0100

    railway webservices fix proper fix now

--- frontend/script.js
+++ frontend/script.js
@@ -23,8 +23,10 @@ function analyseDomain() {
     //loading message
     document.getElementById('result').textContent = "Scanning...";
 
+    const baseURL = "https://final-year-project-25-26-production.up.railway.app/";
+    //baseURL
     //send the request to the backend
-    fetch(`http://localhost:3001/analyse?query=${encodeURIComponent(domain)}`)
+    fetch(`${baseURL}/analyse?query=${encodeURIComponent(domain)}`)
 
         //response into JSON
         .then(response => response.json())
@@ -33,11 +35,14 @@ function analyseDomain() {
             //output string 
             let output = `DOMAIN ANALYSIS\n`;
             output += `Domain: ${data.query}\n`;
-            output += `IP Address: ${data.ip}\n\n`;
+            output += `IP Address: ${data.ip}\n`;
+
+            //Breakpoint Line
+            output += "\n-----------------------------------------------------------------\n";
 
             //MX Records
             //checks if the mxRecords exist
-            output += "MX RECORDS\n";
+            output += "\nMX RECORDS\n";
             if (data.mxRecords && data.mxRecords.length > 0) {
                 data.mxRecords.forEach(mx => {
                     output += `Priority: ${mx.priority}, Exchange: ${mx.exchange}\n`;
@@ -46,6 +51,9 @@ function analyseDomain() {
                 //if there is none
                 output += "No MX records found.\n";
             }
+
+            //Breakpoint Line
+            output += "\n-----------------------------------------------------------------\n";
 
             //NS Records
             output += "\nNAME SERVERS\n";
@@ -56,6 +64,8 @@ function analyseDomain() {
             } else {
                 output += "No NS records found.\n";
             }
+            //Breakpoint Line
+            output += "\n-----------------------------------------------------------------\n";
 
             //SOA Record
             output += "\nSOA RECORD\n";
@@ -67,6 +77,9 @@ function analyseDomain() {
                 output += "No SOA Found.\n";
             }
 
+            //Breakpoint Line
+            output += "\n-----------------------------------------------------------------\n";
+
             //Reverse DNS
             output += "\nREVERSE DNS\n";
             if (data.hostnames && data.hostnames.length > 0) {
@@ -77,6 +90,9 @@ function analyseDomain() {
                 output += "No reverse DNS found.\n";
 
             }
+
+            //Breakpoint Line
+            output += "\n-----------------------------------------------------------------\n";
 
             //TXT Records
             output += "\nTXT Records\n";
@@ -91,6 +107,9 @@ function analyseDomain() {
 
             output += "</details>\n";
 
+            //Breakpoint Line
+            output += "\n-----------------------------------------------------------------\n";
+
             output += "\nDetected Mail Providers\n";
 
             if(data.detectedProviders  && data.detectedProviders.length > 0) {
@@ -100,6 +119,9 @@ function analyseDomain() {
             }else {
                 output += "No providers detected\n";
             }
+
+            //Breakpoint Line
+            output += "\n-----------------------------------------------------------------\n";
   
             
             output += "\nDMARC STATUS\n";
@@ -113,12 +135,19 @@ function analyseDomain() {
                 output += "No Dmarc Found"
             }
 
+            //Breakpoint Line
+            output += "\n-----------------------------------------------------------------\n";
+
                output += "\nEmail Authentication";
                output += "\nDmarc, SPF and DKIM records:\n";
 
             output += `SPF Record: ${data.spfRecord || "Not Found"}\n`;
             output += `DMARC Record: ${data.dmarc || "Not Found"}\n`;
             output += `DKIM ${data.dkim ? "Found" : "Not detected"}\n`
+
+
+            //Breakpoint Line
+            output += "\n-----------------------------------------------------------------\n";
 
 
 
@@ -136,6 +165,10 @@ function analyseDomain() {
             output += `Score: ${data.emailScore}/100\n`
 
 
+            //Breakpoint Line
+            output += "\n-----------------------------------------------------------------\n";
+
+
             output += "\nSecurity Issues\n";
 
             if (data.issues && data.issues.length > 0) {
@@ -146,8 +179,11 @@ function analyseDomain() {
                 output += "\nNo Major Issues Found\n"
             }
 
+            //Breakpoint Line
+            output += "\n-----------------------------------------------------------------\n";
 
-            output += "\nEmail Spoof Security\n"
+
+            output += "\nEmail Security Rating\n"
 
             if (data.spoofAttack) {
                 output += `- Can attackers spoof emails? ${data.spoofAttack.spoofable}\n`;
@@ -155,6 +191,9 @@ function analyseDomain() {
                 output += `- Likely outcome: ${data.spoofAttack.outcome}\n`;
 
             }
+
+            //Breakpoint Line
+            output += "\n-----------------------------------------------------------------\n";
 
             output += "\nOverall Security Rating\n";
 
@@ -169,6 +208,10 @@ function analyseDomain() {
 
             output += `Rating ${overallRating}\n`;
             output += `Score: ${data.overallScore}/100\n`
+
+
+            //Breakpoint Line
+            output += "\n-----------------------------------------------------------------\n";
 
 
 
@@ -192,9 +235,8 @@ window.onload = function() {
     <br>
     In the context of digital forensics and cyber-security,
     it mainly involves collecting data from internet sources which are publicly available to identify threats along with vulnerabilities in digital domains.
-    <p>
+    </p>
 
-    <br>
 
     <h3> IP Address </h3>
     <p>
@@ -208,7 +250,7 @@ window.onload = function() {
 
     <h3> MX Records (Mail Exchange) </h3>
     <p>
-    Mail Exchange (MX) records are a type of DNS (Domain Name System) record that assigns the mail servers responsible for receiving email messages on behalf of a domain name.
+    Mx (Mail Exchange) records are a type of DNS (Domain Name System) record that assigns the mail servers responsible for receiving email messages on behalf of a domain name.
     <br>
     In OSINT and reconnaissance, MX records are extremely valuable as they allow the mapping of an organisation's digital infrastructure,
     identifying email providers and discovering potential security weaknesses without direct interaction with the target network.
@@ -216,35 +258,44 @@ window.onload = function() {
 
     <h3> NS records (Name server) </h3>
     <p>
-    NS records are an important type of DNS (Domain Name System) record that designates which DNS servers are authoritative for a domain.
+    NS records are an important type of DNS record that designates which DNS servers are authoritative for a domain.
     <br>
     They direct internet traffic to the specific servers as they define which servers are responsible for managing the domain's DNS.
     <br>
     In OSINT they are crucial as they reveal critical infrastructure details, hosting providers and potential security weaknesses of a target organisation.
     <br> 
-    <p>
+    </p>
     
     <h3> TXT Records </h3>
     <p>
-    TXT records are a type of DNS (domain) that allows domain admins to insert arbitrary text into their DNS settings.
+    TXT records are a type of DNS that allows domain admins to insert arbitrary text into their DNS settings.
     <br>
     In OSINT investigations, they often reveal critical information about an organisation's infrastructure. 
     They often provide passive intelligence as they can be gathered without direct interaction.
     <br>
+    </p>
+
+    <h3> SOA (Start of Authority) Records </h3>
+    <p>
+    The SOA record is a type of DNS record that contains essential administrative and technical information about a domain.
+    It includes information such as parties involved, primary nameserver and update frequency
+    <br>
+    In OSINT it is important as they provide data about how a domain is managed, which helps to reveal the infrastructure owner or any security misconfigurations.
+    </p>
 
     <h3> SPF Records (Sender Policy Framework) </h3>
     <p>
-    A SPF record is a type of DNS (Domain Name System) TXT record that lists all the servers authorised to send emails from a particular domain.
+    A SPF record is a type of DNS TXT record that lists all the servers authorised to send emails from a particular domain.
     <br>
     In OSINT, it is important as they help give an idea of an organisation's email security and validation of the email during an investigation.
-    <p>
+    </p>
 
-    <h3> DMARC (Domain-Based Message Authentication,Reporting and Conformance) </h3>
+    <h3> DMARC (Domain-Based Message Authentication, Reporting and Conformance) </h3>
     <p>
-    DMARC is a DNS (Domain Name System) TXT record that acts as an email authentication policy. It guides mail servers how to handle emails that fail SPF or DKIM checks.
+    DMARC is a DNS TXT record that acts as an email authentication policy. It guides mail servers how to handle emails that fail SPF or DKIM checks.
     <br>
     It is important in OSINT as it reveals a domain's security posture and it can showcase how an organisation handles phishing or spoofing.
-    <p>
+    </p>
 
     <h3> DKIM (DomainKeys Identified Mail) </h3>
     <p>
@@ -252,11 +303,13 @@ window.onload = function() {
     and that the content within has not been altered.
     <br>
     In OSINT it is vital as to is a way of verifying email legitimacy and email infrastructure.
+    </p>
 
     <h3> Why Does This Matter </h3>
     <p>
     These are records are valuable for OSINT investigations as they help map an organisations infrastructure. If these records are misconfigured, an attacker can spoof emails, send phishing emails and collect valuable information to attack an organisation.
     <br>
+    </p>
 
 
     `
